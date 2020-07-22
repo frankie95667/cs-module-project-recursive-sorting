97617b8ccf6f0f6929c6dda939f2b75582bb96ad
Anthony Kawa <anthonyk2020@gmail.com>
2020-07-21 21:07:29 -0700

    MVP complete, started merge_sort_in_place

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,15 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    midpoint = start + ((end - start) // 2)
+    if midpoint < 0:
+        return -1
+
+    if arr[midpoint] == target:
+        return midpoint
+    elif arr[midpoint] < target:
+        return binary_search(arr, target, midpoint, end)
+    else:
+        return binary_search(arr, target, start, midpoint)
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -11,4 +20,5 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
+    pass
 
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -2,18 +2,39 @@
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-
-    # Your code here
-
-
+    i = j = k = 0
+    while i < len(arrA) and j < len(arrB):
+        if arrA[i] < arrB[j]:
+            merged_arr[k] = arrA[i]
+            i += 1
+        else:
+            merged_arr[k] = arrB[j]
+            j += 1
+        k += 1
+    while i < len(arrA):
+        merged_arr[k] = arrA[i]
+        i += 1
+        k += 1
+    while j < len(arrB):
+        merged_arr[k] = arrB[j]
+        j += 1
+        k += 1
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
+    # Your code here\
+    if(len(arr) <= 1):
+        return arr
+    midpoint = len(arr) // 2
+    L = arr[:midpoint]
+    R = arr[midpoint:]
 
+    L = merge_sort(L)
+    R = merge_sort(R)
 
-    return arr
+    return merge(L, R)
+
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
@@ -21,8 +42,38 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    elements = end
+    merged_arr = [0] * elements
+    i = start
+    j = mid
+    k = 0
+    while i < mid and j < end:
+        if arr[i] < arr[j]:
+            merged_arr[k] = arr[i]
+            i += 1
+        else:
+            merged_arr[k] = arr[j]
+            j += 1
+        k += 1
+    while i < mid:
+        merged_arr[k] = arr[i]
+        i += 1
+        k += 1
+    while j < end:
+        merged_arr[k] = arr[j]
+        j += 1
+        k += 1
+    return merged_arr
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+    print(str(l), str(r))
+    if len(arr[l:r]) <= 1:
+        return arr[l:r]
+    mid = (r - l) // 2
+    merge_sort_in_place(arr, l, mid)
+    merge_sort_in_place(arr, mid + 1, r)
+
+    return merge_in_place(arr, l, mid, r)
 
